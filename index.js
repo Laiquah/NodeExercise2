cfe7c51b02ad9b84c5d7b4bfae79bf4d87facd15
Laiquah <cotielaiquah@gmail.com>
2023-07-31 14:46:41 +0200

    finished exercise 2

---
+++ index.js
@@ -0,0 +1,9 @@
+//Exercise 2
+const p1 = require('./person.js')
+console.log('laiquah', 'cotie', 'cotielaiquah@gmail.com');
+
+const p2 = require('./person.js')
+console.log('ganiefah', 'majiet', 'gmajiet180804@gmail.com');
+
+const p3 = require('./person.js')
+console.log('laeeq', 'majal', 'laeeqmajal@gmail.com');
---
+++ person.js
@@ -0,0 +1,7 @@
+const Person = (firstName, lastName, emailAddress) => {
+    this.name = firstName,
+    this.surname = lastName,
+    this.email = emailAddress
+};
+
+module.exports = Person;
